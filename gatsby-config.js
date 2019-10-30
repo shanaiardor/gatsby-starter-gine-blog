cc33b494617475e28847262e4f5433a5432f3469
shanaiardor <2207561089@qq.com>
2019-10-30 13:55:28 +0800

    Update gatsby-config.js
    config change

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
+                configTable: "https://www.notion.so/shanaiardor/fe416b0e114b4132896e4968fc2eb3ed?v=0e6928554a64444787e42c29d122f44d"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
+                configTable: "https://www.notion.so/shanaiardor/57e0aefe1eb24ff5b283770735909c6e?v=10e1e3e4af9149aab5894162f18d59e4"
             }
         }
     ],
