6f354988278563728021e2a0df5ad3ade7fea848
shanaiardor <2207561089@qq.com>
2019-10-30 14:36:09 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/ebe05119ac8b4a24a49a55bf89d7726f?v=a8eb2dabe1954d159bd30c53f0c84c58"
+                configTable: "https://www.notion.so/shanaiardor/ebe05119ac8b4a24a49a55bf89d7726f?v=a8eb2dabe1954d159bd30c53f0c84c58"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/ed340efa337b4e23b80ef55910114f9e?v=f267e4e9de9046a4b4e511052b931203"
+                configTable: "https://www.notion.so/shanaiardor/ed340efa337b4e23b80ef55910114f9e?v=f267e4e9de9046a4b4e511052b931203"
 "
             }
         }
